55b9bddd69b038192dbea72ecc8aba9367cd5a81
caglary <37529684+caglary@users.noreply.github.com>
2018-04-09 00:26:40 +0300

    Adding BusinessLogicalLayer

---
+++ Mutlu_Ciftci/BusinessLogicalLayer/BLL.cs
@@ -0,0 +1,48 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicalLayer
+{
+    public class BLL
+    {
+        DatabaseLogicalLayer.DLL dll = new DatabaseLogicalLayer.DLL();
+
+        DataTable dt;
+        int ReturnValues;
+      
+        public DataTable Listele()
+        {
+            dll = new DatabaseLogicalLayer.DLL();
+            dt=dll.Listele();
+
+            return dt;
+          
+        }
+
+        public int KayitEkle(Kayitlar K)
+        {
+
+            ReturnValues = dll.KayitEkle(K);
+            return ReturnValues;
+
+        }
+        public int KayitDuzenle(Kayitlar K)
+        {
+            ReturnValues = dll.KayitDuzenle(K);
+            return ReturnValues;
+
+        }
+        public int KayitSil(Kayitlar K)
+        {
+            ReturnValues = dll.KayitKayitSil(K);
+            return ReturnValues;
+
+        }
+
+    }
+}
